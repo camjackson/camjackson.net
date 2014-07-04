45314cb235fa2d5b39fd76a976fb9c14e1439989
Cam Jackson <camjackson89@gmail.com>
2014-07-04 12:12:54 +1000

    Basic express usage

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,16 @@
-//var postHandler = require('./handlers/postHandler');
-//
-//var app = express();
-//app.use(express.static(__dirname + '../../public'));
-//
-//mongoose.connect('mongodb://localhost/test');
-//
-//app.get('/api/posts', postHandler.getPosts);
-//app.get('/api/posts/:post_id', postHandler.getPost);
-//app.post('/api/posts', postHandler.createPost);
-//app.delete('/api/posts/:post_id', postHandler.deletePost);
-//
-//app.listen(8080);
-//
-//console.log('Listening on :8080');
-//exports.app = app;
-//
+var express = require('express');
+var mongoose = require('mongoose');
+var postHandler = require('./handlers/postHandler');
+
+var app = express();
+
+mongoose.connect('mongodb://localhost/writeitdown');
+
+app.get('/api/posts', postHandler.getPosts);
+app.get('/api/posts/:post_id', postHandler.getPost);
+app.post('/api/posts', postHandler.createPost);
+app.delete('/api/posts/:post_id', postHandler.deletePost);
+
+app.listen(8080);
+
+console.log('Listening on :8080');
