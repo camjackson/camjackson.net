43984a86667242b2b46011e05d9ca0c13f31d660
Cam Jackson <camjackson89@gmail.com>
2014-10-05 22:29:45 +1100

    Added endpoint GET /posts/:slug

--- spec/unit/writeitdownSpec.js
+++ spec/unit/writeitdownSpec.js
@@ -14,15 +14,31 @@ describe('WriteItDown', function() {
     it('returns the homepage using the postHandler', function (done) {
       var postHandler = new PostHandler();
       sinon.stub(postHandler, 'getRoot', function(req, res) {
-        res.status(200).send('Hey');
+        res.status(200).send('This is the home page');
       });
 
       request(new WriteItDown(postHandler).app)
         .get('/')
         .end(function(err, res) {
-          expect(err).to.be.null;
           expect(res.statusCode).to.equal(200);
-          expect(res.text).to.equal('Hey');
+          expect(res.text).to.equal('This is the home page');
+          done();
+        });
+    });
+  });
+
+  describe('GET /write/:slug', function() {
+    it('renders the post using the postHandler', function(done) {
+      var postHandler = new PostHandler();
+      sinon.stub(postHandler, 'getPost', function(req, res) {
+        res.status(200).send('This is a single post');
+      });
+
+      request(new WriteItDown(postHandler).app)
+        .get('/posts/some-post')
+        .end(function(err, res) {
+          expect(res.statusCode).to.equal(200);
+          expect(res.text).to.equal('This is a single post');
           done();
         });
     });
@@ -38,7 +54,6 @@ describe('WriteItDown', function() {
       request(new WriteItDown(postHandler).app)
         .get('/write')
         .end(function(err, res) {
-          expect(err).to.be.null;
           expect(res.statusCode).to.equal(200);
           expect(res.text).to.equal('Hi');
           done();
@@ -62,7 +77,6 @@ describe('WriteItDown', function() {
           text: 'Here is some text'
         })
         .end(function (err, res) {
-          expect(err).to.be.null;
           expect(res.statusCode).to.equal(303);
           expect(res.headers.location).to.equal('/posts/some-slug');
           done();
