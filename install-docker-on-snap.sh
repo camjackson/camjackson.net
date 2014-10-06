ab1ec2dee16f8135dd24a0f0c81c8d2fe72b92e3
Cam Jackson <camjackson89@gmail.com>
2014-10-07 00:14:12 +1100

    First pass at docker install script

---
+++ install-docker-on-snap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ ! -d ${SNAP_CACHE_DIR}/docker ]; then
+   echo Docker is not in snap cache dir. Need to download it.
+
+   #Make sure there's only one rpm to copy to cache later
+   rm -f /var/cache/yum/docker-io/packages/*
+
+   sudo yum install docker-io
+
+   echo Copying rpm to snap cache dir for next time.
+   mkdir ${SNAP_CACHE_DIR}/docker
+   mv `find /var/cache/yum/docker-io/packages -name docker*.rpm` ${SNAP_CACHE_DIR}/docker/docker.rpm
+else
+    echo Docker is already downloaded. Just need to install it.
+    rpm -i ${SNAP_CACHE_DIR}/docker/docker.rpm
+fi
+
+echo Starting docker service...
+sudo service docker start
+
+echo Pulling node docker image
+sudo docker pull node:0.10-onbuild #TODO: Cache this too.
