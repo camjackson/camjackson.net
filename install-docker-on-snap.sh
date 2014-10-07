104acccc5d45b4dbede157e977ca2de035827286
Cam Jackson <camjackson89@gmail.com>
2014-10-07 21:43:15 +1100

    Don't try to cache the docker rpm for now...

--- install-docker-on-snap.sh
+++ install-docker-on-snap.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
-if [ ! -e ${SNAP_CACHE_DIR}/docker/docker.rpm ]; then
-   echo Docker is not in snap cache dir. Need to download it.
-
-   #Make sure there's only one rpm to copy to cache later
-   rm -f /var/cache/yum/docker-io/packages/*
-
-   sudo yum -y install docker-io
-
-   echo Copying rpm to snap cache dir for next time.
-   mkdir ${SNAP_CACHE_DIR}/docker
-   mv `find /var/cache/yum/docker-io/packages -name docker*.rpm` ${SNAP_CACHE_DIR}/docker/docker.rpm
-else
-    echo Docker is already downloaded. Just need to install it.
-    rpm -i ${SNAP_CACHE_DIR}/docker/docker.rpm
-fi
+echo Installing docker
+sudo yum -y install docker-io
 
 echo Starting docker service...
+sudo umount cgroup
+sudo service cgconfig start
 sudo service docker start
 
 echo Pulling node docker image
-sudo docker pull node:0.10-onbuild #TODO: Cache this too.
+sudo docker pull node:0.10-onbuild #TODO: Cache this
+
+sudo docker build -t writeitdown
+sudo docker rm writeitdown
+sudo docker run -d --name writeitdown writeitdown
