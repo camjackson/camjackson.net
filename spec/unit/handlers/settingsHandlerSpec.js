ee8c692dfe27eb7a6bd347de49c6d9f61d8dfbf7
Cam Jackson <camjackson89@gmail.com>
2014-11-14 23:34:28 +1100

    Create a settings page, empty for now

---
+++ spec/unit/handlers/settingsHandlerSpec.js
@@ -0,0 +1,43 @@
+var sinon = require('sinon');
+var chai = require('chai');
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+var expect = chai.expect;
+
+var marked = require('marked');
+
+var SettingsHandler = require('../../../lib/handlers/settingsHandler').SettingsHandler;
+var Config = require('../../../lib/models').Config;
+
+describe('SettingsHandler', function() {
+  var sandbox;
+  var response;
+  var settingsHandler = new SettingsHandler(function () {
+    return 'a responder';
+  });
+
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+    sandbox.stub(Config, 'findOne').returns('the config');
+    response = {
+      render: sandbox.spy()
+    };
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  describe('getSettings', function () {
+    it('renders the settings page with the correct data', function () {
+      var request = { flash: sandbox.stub().withArgs('errorMessage').returns('some error') };
+      settingsHandler.getSettings(request, response);
+
+      expect(response.render).to.have.been.calledWith(
+        'settings.jade',
+        { marked: marked, config: 'the config', errorMessage: 'some error' },
+        'a responder'
+      );
+    });
+  });
+});
--- spec/unit/writeitdownSpec.js
+++ spec/unit/writeitdownSpec.js
@@ -8,12 +8,14 @@ var expect = chai.expect;
 var WriteItDown = require('../../lib/writeitdown').WriteItDown;
 var AuthHandler = require('../../lib/handlers/authHandler').AuthHandler;
 var UserHandler = require('../../lib/handlers/userHandler').UserHandler;
+var SettingsHandler = require('../../lib/handlers/settingsHandler').SettingsHandler;
 var PostHandler = require('../../lib/handlers/postHandler').PostHandler;
 
 describe('WriteItDown', function() {
   var sandbox;
   var authHandler = new AuthHandler();
   var userHandler = new UserHandler();
+  var settingsHandler = new SettingsHandler();
   var postHandler = new PostHandler();
 
   beforeEach(function() {
@@ -123,6 +125,33 @@ describe('WriteItDown', function() {
         .end(function(err, res) {
           expect(res.statusCode).to.equal(200);
           expect(res.text).to.equal("This is the current user's profile");
+          done();
+        });
+    });
+  });
+
+  describe('GET /settings', function () {
+    it('redirects to the login page when the user is not authenticated', function(done) {
+      request(new WriteItDown({settingsHandler: settingsHandler, authHandler: authHandler}).app)
+        .get('/settings')
+        .end(function(err, res) {
+          expect(res.statusCode).to.equal(303);
+          expect(res.headers.location).to.equal('/login');
+          done();
+        });
+    });
+
+    it('renders the settings page using the settingsHandler when the user is authenticated', function (done) {
+      authorise();
+      sandbox.stub(settingsHandler, 'getSettings', function(req, res) {
+        res.status(200).send("This is the edit settings page");
+      });
+
+      request(new WriteItDown({settingsHandler: settingsHandler, authHandler: authHandler}).app)
+        .get('/settings')
+        .end(function(err, res) {
+          expect(res.statusCode).to.equal(200);
+          expect(res.text).to.equal("This is the edit settings page");
           done();
         });
     });
