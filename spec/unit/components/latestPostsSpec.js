d71f34c31f9702c22ef1df6a2fdabf2fc1bdf821
Cam Jackson <camjackson89@gmail.com>
2015-11-17 17:28:52 +1100

    Render real post previews on new frontpage

--- spec/unit/components/latestPostsSpec.js
+++ spec/unit/components/latestPostsSpec.js
@@ -6,7 +6,11 @@ const LatestPosts = require('../../../lib/components/latestPosts');
 
 describe('LatestPosts', function() {
   it('renders 2 posts', function() {
-    const latestPosts = shallowRender(<LatestPosts/>);
-    expect(latestPosts.props.children[1].props.children.length).to.equal(2);
+    const posts = ['post1', 'post2'];
+    const latestPosts = shallowRender(<LatestPosts posts={posts}/>);
+    const postPreviews = latestPosts.props.children[1].props.children;
+
+    expect(postPreviews[0].props.post).to.equal('post1');
+    expect(postPreviews[1].props.post).to.equal('post2');
   });
 });
--- spec/unit/components/postPreviewSpec.js
+++ spec/unit/components/postPreviewSpec.js
@@ -5,15 +5,16 @@ const shallowRender = require('./shallowRender');
 const PostPreview = require('../../../lib/components/postPreview');
 
 describe('PostPreview', function() {
-  const post = {title: 'My Post', slug: 'my-post', blurb: 'This is a post'};
+  const post = {title: 'My Post', slug: 'my-post', blurb: '**This is a post**'};
   const postPreview = shallowRender(<PostPreview post={post}/>);
 
   it('renders the title', function() {
     expect(postPreview.props.children[0].props.children).to.equal('My Post');
   });
 
-  it('renders the blurb', function() {
-    expect(postPreview.props.children[1].props.children[0]).to.equal('This is a post');
+  it('transforms and renders the blurb', function() {
+    const blurb = postPreview.props.children[1].props.children[0];
+    expect(blurb.props.dangerouslySetInnerHTML.__html).to.equal('<p><strong>This is a post</strong></p>\n');
   });
 
   it('renders a link to the post', function() {
