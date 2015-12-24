4df999235928043797f44e3b6bd0523f00afc51a
Cam Jackson <camjackson89@gmail.com>
2015-12-24 18:12:14 +1100

    And then make it a bit prettier on large screens

--- src/components/jumbo.js
+++ src/components/jumbo.js
@@ -7,13 +7,15 @@ module.exports = () => (
     <div className="container">
       <div className="row">
         <div className="col-xs-10 col-xs-offset-1
-                        col-sm-7 col-sm-offset-5">
+                        col-sm-7 col-sm-offset-5
+                        col-md-6 col-md-offset-6">
           <h1>Cam Jackson</h1>
         </div>
       </div>
       <div className="row">
         <div className="col-xs-6 col-xs-offset-1
-                        col-sm-push-4">
+                        col-sm-push-4
+                        col-md-6 col-md-offset-2">
           <h3>- Full stack developer</h3>
           <h3>- DevOps evangelist</h3>
           <h3>- Consultant</h3>
