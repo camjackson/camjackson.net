172de992891a6b4e9ce8946b40d67fcae1884123
Cam Jackson <camjackson89@gmail.com>
2017-04-14 12:51:15 +1000

    Refactor the atom feed rendering so I can reuse it elsewhere

--- src/getFeed.js
+++ src/getFeed.js
@@ -11,54 +11,59 @@ marked.setOptions({
   }
 });
 
+const renderFeedXml = posts => {
+  const formatDate = date => moment(date).format();
+  const link = (rel, href) => ([
+    { _attr: {rel: rel} },
+    { _attr: {href: href} }
+  ]);
+  const renderMarkdown = markdown => ({
+    _attr: { type: 'html' },
+    _cdata: marked(markdown)
+  });
+
+  const me = [
+    { name: 'Cam Jackson' },
+    { uri: 'https://camjackson.net/' }
+  ];
+
+  const feed = [
+    { _attr: { xmlns: 'http://www.w3.org/2005/Atom' } },
+    { id: 'https://camjackson.net/' },
+    { title: 'camjackson.net' },
+    { updated: formatDate(posts[0].posted) },
+    { subtitle: 'A feed of blog posts from camjackson.net' },
+    { author: me },
+    { link: link('alternate', 'https://camjackson.net/')},
+    { link: link('self', 'https://camjackson.net/atom.xml')},
+    { icon: 'https://camjackson.net/favicon.ico' },
+    { logo: 'https://camjackson.net/profile.jpg' }
+  ];
+
+  posts.forEach(post => {
+    const uri = `https://camjackson.net/post/${post.slug}`;
+    feed.push({
+      entry: [
+        { id: uri },
+        { title: {_cdata: post.title} },
+        { updated: formatDate(post.posted) },
+        { published: formatDate(post.posted) },
+        { author: me },
+        { content: renderMarkdown(post.text) },
+        { link: link('alternate', uri)},
+        { summary: renderMarkdown(post.text.substr(0, post.text.indexOf('[//]: # (fold)'))) }
+      ]
+    });
+  });
+  return xml({feed}, {indent: true, declaration: true});
+}
+
 module.exports = (_, res) => {
   const attrsGet = ['slug', 'title', 'posted', 'text'];
-  return Posts.scan({attrsGet}).then((posts) => {
-    const format = date => moment(date).format();
-    const link = (rel, href) => ([
-      { _attr: {rel: rel} },
-      { _attr: {href: href} }
-    ]);
-    const render_markdown = markdown => ({
-      _attr: { type: 'html' },
-      _cdata: marked(markdown)
+  return Posts.scan({attrsGet})
+    .then(renderFeedXml)
+    .then(feed => {
+      res.set('Content-Type', 'application/atom+xml');
+      res.send(feed);
     });
-
-    const me = [
-      { name: 'Cam Jackson' },
-      { uri: 'https://camjackson.net/' }
-    ];
-
-    const feed = [
-      { _attr: { xmlns: 'http://www.w3.org/2005/Atom' } },
-      { id: 'https://camjackson.net/' },
-      { title: 'camjackson.net' },
-      { updated: format(posts[0].posted) },
-      { subtitle: 'A feed of blog posts from camjackson.net' },
-      { author: me },
-      { link: link('alternate', 'https://camjackson.net/')},
-      { link: link('self', 'https://camjackson.net/atom.xml')},
-      { icon: 'https://camjackson.net/favicon.ico' },
-      { logo: 'https://camjackson.net/profile.jpg' }
-    ];
-
-    posts.forEach(post => {
-      const uri = `https://camjackson.net/post/${post.slug}`;
-      feed.push({
-        entry: [
-          { id: uri },
-          { title: {_cdata: post.title} },
-          { updated: format(post.posted) },
-          { published: format(post.posted) },
-          { author: me },
-          { content: render_markdown(post.text) },
-          { link: link('alternate', uri)},
-          { summary: render_markdown(post.text.substr(0, post.text.indexOf('[//]: # (fold)'))) }
-        ]
-      });
-    });
-
-    res.set('Content-Type', 'application/atom+xml');
-    res.send(xml({feed}, {indent: true, declaration: true}));
-  });
 };
