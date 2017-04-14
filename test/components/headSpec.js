e3d94761318ae1c4fd7340e23aef499c689c2f37
Cam Jackson <camjackson89@gmail.com>
2017-04-14 12:25:32 +1000

    Fix component assertions in tests

--- test/components/headSpec.js
+++ test/components/headSpec.js
@@ -7,12 +7,12 @@ const Head = require('../../src/components/head');
 describe('Head', () => {
   it('has my name as the title by default', () => {
     const head = shallowRender(<Head/>);
-    expect(head.props.children[8].props.children).to.equal('Cam Jackson');
+    expect(head.props.children[7].props.children).to.equal('Cam Jackson');
   });
 
   it('supports title overrides, with my name as a suffix', () => {
     const head = shallowRender(<Head title="Custom Title"/>);
-    expect(head.props.children[8].props.children).to.equal('Custom Title - Cam Jackson');
+    expect(head.props.children[7].props.children).to.equal('Custom Title - Cam Jackson');
   });
 
   it('includes extra page scripts if they are given', () => {
